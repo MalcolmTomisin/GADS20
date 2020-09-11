ee9e018e3efcf6a33b060714fe1e7043535f1252
Oluwatomisin <malcolmtomisin@gmail.com>
2020-09-11 22:53:38 +0100

    refactor views, widgets

--- app/src/main/java/com/alc/leaderboard/SubmissionActivity.java
+++ app/src/main/java/com/alc/leaderboard/SubmissionActivity.java
@@ -5,7 +5,9 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -30,17 +32,22 @@ public class SubmissionActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.custom_toolbar_submission);
+        View view = getSupportActionBar().getCustomView();
+        ImageView imageView = findViewById(R.id.imageView3);
+        imageView.setOnClickListener((v) -> {
+            finish();
+        });
 
         binding.button2.setOnClickListener((v) -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(SubmissionActivity.this);
             LayoutInflater layoutInflater = getLayoutInflater();
             builder.setView(layoutInflater.inflate(R.layout.dialog_action, null));
             AlertDialog dialog = builder.create();
+            dialog.show();
             Button button = dialog.findViewById(R.id.button3);
             button.setOnClickListener((b) -> {
                 submit();
             });
-            dialog.show();
         });
 
     }
